74b713f1acc7d070fd10c232b4e98123ea1575a1
madan0511 <madan.mohanan@diatoz.com>
2020-11-30 23:25:37 +0530

    mail send

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask,render_template,request,redirect,url_for
 import os
 from flask_cors import CORS, cross_origin
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def send_mail(name, msg, email, subject):
+    try:
+        #The mail addresses and password
+        sender_address = 'kkgoldmart@gmail.com'
+        sender_pass = 'wiqzdktewlljyjya'
+        receiver_address = 'kkgoldmart@gmail.com'
+        #Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = name +" - "+email
+        message['To'] = receiver_address
+        message['Subject'] = subject  #The subject line
+        #The body and the attachments for the mail
+        message.attach(MIMEText(msg, 'plain'))
+        #Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+        session.starttls() #enable security
+        session.login(sender_address, sender_pass) #login with mail_id and password
+        text = message.as_string()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
+        print('Mail Sent')
+    except:
+        pass
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -20,6 +48,15 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route('/sendMail',methods=["POST","GET"])
+def sendMail():
+    username = request.form['username']
+    email = request.form['email']
+    subject = request.form['subject']
+    message = request.form['message']
+    send_mail(username,message,email,subject)
+    return "success"
+    
 @app.route('/estimator',methods=['GET','POST'])
 def estimator():
     try:
@@ -59,4 +96,4 @@ def changeRate():
     return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8081)
+    app.run(debug=True,port=8080,host='0.0.0.0')
